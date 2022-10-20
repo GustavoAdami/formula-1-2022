0477c91da0db24481f2137581db7aff43001ef58
Gustavo Adami <g>
2022-10-20 11:35:29 -0400

    seb audio, logger

--- src/app.js
+++ src/app.js
@@ -1,24 +1,32 @@
 const { F122UDP } = require("f1-22-udp");
-const { networkInterfaces } = require("os");
-const nets = networkInterfaces();
+const { getLocalIPv4 } = require('./utils/utils');
+const { logger } = require('./utils/logger');
+const player = require("play-sound")((opts = {}));
 
 const f122 = new F122UDP({
   address: getLocalIPv4(),
 });
 
 /* Telemetry Toggles */
-const readMotion = true;
-const readSession = true;
-const readLapData = true;
-const readEvent = true;
-const readParticipants = true;
-const readCarSetups = true;
-const readCarTelemetry = true;
-const readCarStatus = true;
-const readFinalClassification = true;
-const readLobbyInfo = true;
-const readCarDamage = true;
-const readSessionHistory = true;
+const readMotion = true;              /* Contains all motion data for player’s car – only sent while player is in control */
+const readSession = true;             /* Data about the session – track, time left */
+const readLapData = true;             /* Data about all the lap times of cars in the session */
+const readEvent = true;               /* Various notable events that happen during a session */
+const readParticipants = true;        /** List of participants in the session, mostly relevant for multiplayer */
+const readCarSetups = true;           /** Packet detailing car setups for cars in the race */
+const readCarTelemetry = true;        /** Telemetry data for all cars */
+const readCarStatus = true;           /** Status data for all cars */
+const readFinalClassification = true; /** Final classification confirmation at the end of a race */
+const readLobbyInfo = true;           /** Information about players in a multiplayer lobby */
+const readCarDamage = true;           /** Damage status for all cars */
+const readSessionHistory = true;      /** Lap and tyre data for session */
+
+/* Player Properties */
+let driverName = "";
+let driverIndex = -1;
+let currentPosition = -1;
+let carIndex = -1;
+let overtake = -1;
 
 try {
   f122.start();
@@ -37,6 +45,9 @@ if (readMotion) {
 if (readEvent) {
   f122.on("event", (eventData) => {
     console.log("EVENT", eventData);
+    /*
+     * TODO: Lights out - Audio
+     */
   });
 }
 
@@ -72,59 +83,83 @@ if (readFinalClassification) {
 
 if (readLapData) {
   f122.on("lapData", (lapData) => {
-    console.log("LAP DATA", lapData);
+    logger.log('LAP DATA', lapData);
+    if (currentPosition === -1 && lapData.m_lapData[carIndex]) {
+      console.log("LAP DATA", lapData.m_lapData[carIndex]);
+
+      currentPosition = lapData.m_lapData[carIndex].m_carPosition;
+      console.log("Current position: " + currentPosition);
+
+      return;
+    }
+
+    if (currentPosition !== lapData.m_lapData[carIndex].m_carPosition) {
+      if (overtake === -1) {
+        overtake = 0;
+      }
+
+	  /* Overtake condition */
+      if (lapData.m_lapData[carIndex].m_carPosition < currentPosition) {
+
+		/**
+		 * TODO: Put audio in a queue to play them in sequence and not cut the one that is playing.
+		 * TODO: Several audios per driver. Random
+		 */
+        player.play("res/vettel-trimmed.mp3", function (err) {
+          if (err) throw err;
+        });
+      }
+
+      console.log(
+        "CHANGE POSITION! Old: " +
+          currentPosition +
+          " New: " +
+          lapData.m_lapData[carIndex].m_carPosition
+      );
+      currentPosition = lapData.m_lapData[carIndex].m_carPosition;
+    }
   });
 }
 
 if (readLobbyInfo) {
-  f122.on("lobbyInfo", (lobbyInfoData) => {
-    console.log("LOBBY INFO", lobbyInfoData);
+  f122.on("lobbyInfo", (lobbyData) => {
+    logger.log('LOBBY INFO', lobbyData);
+    console.log("LOBBY INFO", lobbyData);
   });
 }
 
 if (readParticipants) {
   f122.on("participants", (participantsData) => {
-    console.log("PARTICIPANTS", participantsData);
+    logger.log('PARTICIPANTS', participantsData);
+    console.log('PARTICIPANTS', participantsData);
+
+	/* Update driver properties */
+    participantsData.m_participants.forEach((participant) => {
+      if (driverName === "" && participant.m_name === "VETTEL") {
+        console.log("Updating Driver Name and Index");
+        driverName = participant.m_name;
+        driverIndex = participant.m_driverId;
+        carIndex = participantsData.m_header.m_playerCarIndex;
+      }
+    });
   });
 }
 
 if (readSession) {
   f122.on("session", (sessionData) => {
+    logger.log('SESSION', sessionData);
     console.log("SESSION", sessionData);
   });
 }
 
 if (readSessionHistory) {
   f122.on("sessionHistory", (sessionHistoryData) => {
+    logger.log('SESSION HISTORY', sessionHistoryData);
     console.log("SESSION HISTORY", sessionHistoryData);
   });
 }
 
 f122.on("error", (errorData) => {
+  logger.error('ERROR', errorData);
   console.log("ERROR", errorData);
 });
-
-function getNetworkInterfaces() {
-  /* https://stackoverflow.com/questions/3653065/get-local-ip-address-in-node-js */
-  const results = Object.create(null); // Or just '{}', an empty object
-
-  for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-      // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
-      const familyV4Value = typeof net.family === "string" ? "IPv4" : 4;
-      if (net.family === familyV4Value && !net.internal) {
-        if (!results[name]) {
-          results[name] = [];
-        }
-        results[name].push(net.address);
-      }
-    }
-  }
-
-  return results;
-}
-
-function getLocalIPv4() {
-  return getNetworkInterfaces()['Wi-Fi'][0];
-}
---
+++ src/utils/logger.js
@@ -0,0 +1,26 @@
+const { Console } = require("console");
+const fs = require("fs");
+
+// make a new logger
+// const logger = new Console({
+//     stdout: fs.createWriteStream("logs/logs.txt"),
+//     stderr: fs.createWriteStream("logs/error_logs.txt"),
+//   });
+  
+  // // saving to normalStdout.txt file
+  // myLogger.log("Hello 😃. This will be saved in normalStdout.txt file");
+  
+  // // saving to errStdErr.txt file
+  // myLogger.error("Its an error ❌. This will be saved in errStdErr.txt file");
+
+  const logger = (sessionId) => {
+    const log = new Console({
+        stdout: fs.createWriteStream(`logs/logs_${sessionId}.txt`),
+        stderr: fs.createWriteStream(`logs/error_logs_${sessionId}.txt`),
+      });
+
+
+    return log;
+  } 
+
+  exports.logger = logger;
